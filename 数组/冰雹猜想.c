08136b616e878772e6f8c92a26dbc270ee0b5fd4
calistalearns <1615699158@qq.com>
2025-12-25 23:57:26 +0800

    调整

--- 数组/冰雹猜想.c
+++ 数组/冰雹猜想.c
@@ -3,29 +3,27 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int a=n;
+
     //我拿数组把他们存起来？然后倒序输出？？
     int count=0;
     int arr[1000];
+     arr[count]=n;
     while(n!=1)
     {
         if(n%2!=0)
         {
-            n=n*3+1;
-            count++;
-            arr[count]=n;         
+            n=n*3+1;        
         }
         else
         {
             n/=2;
-            count++;
-            arr[count]=n;
         }
+        count++;
+        arr[count]=n;
     }
-    for(int i=count;i>0;i--)
+    for(int i=count;i>=0;i--)
     {
         printf("%d ",arr[i]);
     }
-    printf("%d\n",a);
     return 0;
 }
